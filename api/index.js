3e2c925868a9444adf275849492aa88f3a538b2f
mcontrerasglobal <mcontreras@globalultrasonido.cl>
2025-02-11 12:27:02 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET');
+    res.setHeader('Access-Control-Allow-Origin', 'https://globalultrasonido.cl');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.method === 'OPTIONS') {
